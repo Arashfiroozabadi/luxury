980257664e9813059e812a30f3e5f8c6a4250edd
arashfiroozabadi <arashfiroozabadii@gmail.com>
2019-10-09 05:25:32 +0330

    bug fix in upload page

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -2,14 +2,84 @@ import React, {
     useState,
     useEffect
 } from 'react';
+import clsx from 'clsx';
+import { TextField, Button, Paper, Grid, FormHelperText, Typography, Container, MenuItem, LinearProgress } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import axios from 'axios';
+
 import Layout from '../components/Layout';
+import RTL from '../components/RTL';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            [theme.breakpoints.only('xs')]: {
+                width: '100%',
+            },
+        },
+        uploadRoot: {
+
+        },
+        loginRoot: {
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'column',
+        },
+        formTitle: {
+            width: '80%',
+        },
+        textField: {
+            width: '80%',
+        },
+        dense: {
+            marginTop: theme.spacing(2),
+        },
+        sendButton: {
+            width: '40%',
+            marginTop: theme.spacing(1),
+        },
+        formFooter: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+        progress: {
+            width: '80%',
+            marginBottom: theme.spacing(1),
+        },
+        img: {
+            width: '80%',
+            height: 'auto',
+            borderRadius: 4,
+            boxShadow: theme.shadows['10']
+        },
+        sendFormButton: {
+            width: '50%',
+            marginTop: theme.spacing(1),
+        },
+        helperText: {
+            width: '90%',
+            height: 30,
+            padding: theme.spacing(1)
+        }
+    })
+)
 
 function Upload() {
+    const classes = useStyles()
     const [data, setData] = useState<any | null>({});
+    const [load, setLoad] = useState<any | null>({ loaded: 0 });
+    const [err, setErr] = useState(false);
     const [form, setForm] = useState<any | null>({
         userName: '',
         pass: '',
+        msg: ""
+    });
+    const [uploadForm, setUpload] = useState<any | null>({
+        name: '',
+        desc: '',
+        cate: '',
+        // file: {},
         msg: ""
     });
     const [isAuth, setAuth] = useState()
@@ -30,54 +100,247 @@ function Upload() {
     }
 
     function handleSend() {
-        axios.post('/api/upload', {
+        axios.post('/api/login', {
             form
         }).then((res: any) => {
             setData(res.data)
+            setErr(!res.data.auth)
         })
     }
+
+    function handleChangeUpload(e: any) {
+        setUpload({ ...uploadForm, [e.target.name]: e.target.value })
+    }
+    function handleChangeImage(e: any) {
+        setUpload({ ...uploadForm, img: e.target.files[0] })
+    }
+    function handleSendFile() {
+        const file = new FormData()
+        // const regex = new RegExp('[^.]+$');
+        // const extension = uploadForm.img.name.match(regex)[0];
+
+        file.append('name', uploadForm.name)
+        file.append('desc', uploadForm.desc)
+        file.append('cate', uploadForm.cate)
+        file.append('file', uploadForm.img)
+
+        axios.post('/api/upload',
+            file, {
+            onUploadProgress: ProgressEvent => {
+                setLoad({
+                    loaded: (ProgressEvent.loaded / ProgressEvent.total * 100),
+                })
+            },
+        }
+        ).then((res: any) => {
+            setData(res.data)
+            console.log(res.data);
+
+            // setErr(!res.data.auth)
+        })
+    }
+    const cate = [
+        {
+            value: 'راحتی',
+            label: 'راحتی',
+        },
+        {
+            value: 'راحتی-ال',
+            label: 'راحتی-ال',
+        },
+        {
+            value: 'سرویس خواب',
+            label: 'سرویس خواب',
+        },
+        {
+            value: 'نهار خوری',
+            label: 'نهار خوری',
+        },
+    ];
+    console.log(data.auth);
 
     if (data.auth === true) {
         return (
             <Layout>
-                {data.msg}
-                <br />
-                <button
-                    onClick={() => handleSend()}
-                >
-                    test
-                </button>
-            </Layout>
+                <RTL>
+                    <Container>
+                        <Paper>
+                            <Grid
+                                container
+                                justify="center"
+                            >
+                                <Grid
+                                    item
+                                    xs={12}
+                                >
+                                    <Typography
+                                        variant="h5"
+                                        component="h6"
+                                        className={classes.formTitle}
+                                    >
+                                        ارسال اطلاعات محصول
+                                        </Typography>
+                                </Grid>
+                                <Grid
+                                    item
+                                    xs={4}
+                                >
+                                    <TextField
+                                        className={clsx(classes.textField, classes.dense)}
+                                        name='name'
+                                        label="نام"
+                                        onChange={(e) => handleChangeUpload(e)}
+                                        type="text"
+                                        margin="dense"
+                                        variant="outlined"
+                                    />
+                                    <TextField
+                                        className={clsx(classes.textField, classes.dense)}
+                                        select
+                                        label="انتخاب دسته بندی"
+                                        name="cate"
+                                        value={uploadForm.cate}
+                                        onChange={(e) => handleChangeUpload(e)}
+                                        margin="dense"
+                                        variant="outlined"
+                                    >
+                                        {cate.map(option => (
+                                            <MenuItem key={option.value} value={option.value}>
+                                                {option.label}
+                                            </MenuItem>
+                                        ))}
+                                    </TextField>
+                                    <TextField
+                                        className={clsx(classes.textField, classes.dense)}
+                                        name='desc'
+                                        label="توضیحات"
+                                        onChange={(e) => handleChangeUpload(e)}
+                                        type="text"
+                                        margin="dense"
+                                        multiline
+                                        rows="4"
+                                        rowsMax="6"
+                                        variant="outlined"
+                                    />
+                                </Grid>
+                                <Grid
+                                    item
+                                    xs={6}
+                                >
+                                    <TextField
+                                        className={clsx(classes.textField, classes.dense)}
+                                        name='file'
+                                        onChange={(e) => handleChangeImage(e)}
+                                        type="file"
+                                        margin="dense"
+                                        variant="outlined"
+                                    />
+                                    <LinearProgress
+                                        className={classes.progress}
+                                        variant="determinate"
+                                        value={load.loaded}
+                                    />
+                                    <img
+                                        className={classes.img}
+                                        src={data.imgPath}
+                                    />
+                                </Grid>
+                                <Grid
+                                    item
+                                    xs={12}
+                                    className={classes.formFooter}
+                                >
+                                    <Button
+                                        className={classes.sendFormButton}
+                                        onClick={() => handleSendFile()}
+                                        color="primary"
+                                        variant="contained"
+                                    >
+                                        ارسال
+                                    </Button>
+                                    <FormHelperText
+                                        className={classes.helperText}
+                                        color="red"
+                                        style={data.status === 'err' ?
+                                            {
+                                                color: 'red'
+                                            }
+                                            :
+                                            {
+                                                color: 'green'
+                                            }
+                                        }
+                                    >
+                                        {data.msg}
+                                    </FormHelperText>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Container>
+                </RTL>
+            </Layout >
         )
     } else
         return (
             <Layout>
-                {data.msg}
-                <br />
-                <div>
-                    <input
-                        name='userName'
-                        onChange={(e) => handleChange(e)}
-                        type="text"
-                    />
-                    <br />
-                    <input
-                        name='pass'
-                        onChange={(e) => handleChange(e)}
-                        type="text"
-                    />
-                </div>
-                <button
-                    onClick={() => handleSend()}
-                >
-                    test
-                </button>
-                <p>
-                    {form.userName}
-                </p>
-                <p>
-                    {form.pass}
-                </p>
+                <RTL>
+                    <Container>
+                        <Grid
+                            container
+                            justify="center"
+                        >
+                            <Grid
+                                item
+                                xs={3}
+                            >
+                                <Paper
+                                    className={classes.loginRoot}
+                                    elevation={8}
+                                >
+                                    <Typography
+                                        variant="h5"
+                                        component="h6"
+                                        className={classes.formTitle}
+                                    >
+                                        ورود
+                                </Typography>
+                                    <TextField
+                                        className={clsx(classes.textField, classes.dense)}
+                                        name='userName'
+                                        label="نام کاربری"
+                                        onChange={(e) => handleChange(e)}
+                                        type="text"
+                                        margin="dense"
+                                        variant="outlined"
+                                    />
+                                    <TextField
+                                        className={clsx(classes.textField, classes.dense)}
+                                        name='pass'
+                                        label="رمزعبور"
+                                        onChange={(e) => handleChange(e)}
+                                        type="text"
+                                        margin="dense"
+                                        variant="outlined"
+                                    />
+                                    <Button
+                                        className={classes.sendButton}
+                                        onClick={() => handleSend()}
+                                        color="primary"
+                                        variant="contained"
+                                    >
+                                        ارسال
+                                </Button>
+                                    <FormHelperText
+                                        className={classes.helperText}
+                                        error={err}
+                                    >
+                                        {data.msg}
+                                    </FormHelperText>
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </RTL>
             </Layout>
         )
 }
--- server/api/index.tsx
+++ server/api/index.tsx
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import mkdirp from 'mkdirp';
 import {
     User,
     PhotoModel
@@ -17,63 +18,99 @@ AccountController.get('/all', (_req, res) => {
 })
 
 var storage = multer.diskStorage({
-    destination: function (_req: any, _file: any, cb) {
-        cb(null, 'static/uploads')
+    destination: function (req: any, _file: any, cb) {
+        const dest = `static/uploads/${req.body.cate}`
+        mkdirp.sync(dest);
+        cb(null, dest)
     },
     filename: function (_req: any, file, cb) {
         cb(null, file.originalname)
+        console.log(_req.body.name);
+
     }
 })
 var upload = multer({ storage: storage })
 AccountController.post('/auth', (req: any, res) => {
-    console.log(req.session);
     if (req.session.auth) {
         res.send({ auth: true, test: req.session.auth, msg: 'وارد شدید' })
     } else {
         res.send({ auth: false })
     }
 })
-AccountController.post('/upload', upload.single('file'), (req: any, res) => {
+AccountController.post('/login', upload.single('file'), (req: any, res) => {
     const body = req.body.form
+    console.log(req.body);
 
     if (body.userName.length <= 0) {
-        res.send({ auth: false, msg: "نام کاربری را وارد کنید" })
+        res.send({ msg: "نام کاربری را وارد کنید" })
     } else if (body.pass.length <= 0) {
-        res.send({ auth: false, msg: "رمز عبور را وارد کنید" })
+        res.send({ msg: "رمز عبور را وارد کنید" })
     } else {
         if (req.session.auth == undefined) {
             User.findOne({ userName: body.userName, pass: body.pass }).then(
                 (resualt: any) => {
                     if (resualt === null) {
-                        res.send({ auth: false, msg: 'نام کاربری و یا رمز عبور اشتباه است' })
+                        res.send({ msg: 'نام کاربری و یا رمز عبور اشتباه است' })
                     } else {
                         req.session.auth = { username: body.userName };
                         res.send({ auth: true, userName: resualt.userName, msg: 'وارد شدید' })
                     }
                 }
             )
+        } else if (req.session.auth.username === body.userName) {
+            res.send({ auth: true, userName: body.userName, msg: 'وارد شدید' })
         }
     }
+})
+AccountController.post('/upload', upload.single('file'), (req: any, res) => {
 
-    // req.session!.auth = { username : req.body['username'] } ;
-    // const file = req.file
-    // console.log(req.body);
+    const file = req.file;
+    const body = req.body;
 
-    // const newPhoto = new PhotoModel({
-    //     title: file.filename,
-    //     path: file.path,
-    // })
-    // newPhoto.save();
-    // res.send(file.path)
-    // if (req.body.name === 'arash') {
+    if (body.name.length === 0 || body.cate.length === 0 || body.desc.length === 0) {
+        console.log('file name is null');
+        res.send({ auth: true, msg: 'لطفا تمام فیلد‌ها را وارد کنید', status: 'err' })
+    } else {
+        const newPhoto = new PhotoModel({
+            title: body.name,
+            category: body.cate,
+            description: body.desc,
+            path: file.path
+        })
+        newPhoto.save();
 
-    //     const body = req.body
-    //     User.findOne({ userName: body.name }).then(
-    //         (resualt: any) => {
-    //             console.log(resualt.userName);
-    //             res.send(resualt)
-    //         }
-    //     )
+        res.send({
+            msg: "ثبت شد",
+            auth: true,
+            imgPath: file.path,
+            status: 'ok'
+        });
+    }
+    console.log(
+        typeof (body.name)
+    );
+
+    // res.send({ auth: true, imgPath: req.file.path })
+
+    // if (body.userName.length <= 0) {
+    //     res.send({ msg: "نام کاربری را وارد کنید" })
+    // } else if (body.pass.length <= 0) {
+    //     res.send({ msg: "رمز عبور را وارد کنید" })
+    // } else {
+    //     if (req.session.auth == undefined) {
+    //         User.findOne({ userName: body.userName, pass: body.pass }).then(
+    //             (resualt: any) => {
+    //                 if (resualt === null) {
+    //                     res.send({ msg: 'نام کاربری و یا رمز عبور اشتباه است' })
+    //                 } else {
+    //                     req.session.auth = { username: body.userName };
+    //                     res.send({ auth: true, userName: resualt.userName, msg: 'وارد شدید' })
+    //                 }
+    //             }
+    //         )
+    //     } else if (req.session.auth.username === body.userName) {
+    //         res.send({ auth: true, userName: body.userName, msg: 'وارد شدید' })
+    //     }
     // }
 })
 
