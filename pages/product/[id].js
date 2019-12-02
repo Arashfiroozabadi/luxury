f4aeb2d05bb9c06b1cc481e3a675e52c931907bc
arashfiroozabadi <arashfiroozabadii@gmail.com>
2019-12-02 11:21:40 +0330

    create product page

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -3,6 +3,14 @@ import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
 // import { useSelector } from 'react-redux'
 import axios from 'axios';
+import {
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  Box
+} from '@material-ui/core';
 
 export default function Post() {
   // const data = useSelector(state => state.data)
@@ -26,8 +34,41 @@ export default function Post() {
 
   return (
     < Layout >
-      <h1>{res.title}</h1>
-      <p>{res.description}</p>
+      <Container>
+        <Grid container justify="center" >
+          <Grid item
+            md={12}
+
+          >
+            <Card>
+              <CardContent>
+                <Typography
+                  variant="h6"
+                  component="h2"
+                  gutterBottom
+                // className={classes.title}
+                >
+                  <Box>
+                    {`مدل ${res.title}`}
+                  </Box>
+                </Typography>
+              </CardContent>
+              <CardContent>
+                <Typography
+                  variant="h6"
+                  component="h5"
+                  gutterBottom
+                // className={classes.title}
+                >
+                  <Box>
+                    {`مدل ${res.description}`}
+                  </Box>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
     </Layout >
   );
 }
