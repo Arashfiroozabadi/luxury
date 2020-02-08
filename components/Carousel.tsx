58578b244d4035e4956ba0d2f4695cfa28ebbdf6
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-02-08 18:39:02 +0330

    add productLink in banner

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import MobileStepper from '@material-ui/core/MobileStepper';
 import Paper from '@material-ui/core/Paper';
+import clsx from 'clsx';
 // import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
@@ -25,6 +26,18 @@ const useStyles = makeStyles(theme => ({
         height: 50,
         backgroundColor: theme.palette.background.default,
     },
+    imgContainer: {
+        position: 'relative',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
+        '&:hover': {
+            '& .productLink': {
+                transform: 'translateY(-10px)',
+            }
+        }
+    },
     img: {
         height: 300,
         display: 'block',
@@ -37,6 +50,22 @@ const useStyles = makeStyles(theme => ({
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center center',
     },
+    productLink: {
+        color: 'gold',
+        width: 'calc(100% - 20px);',
+        bottom: 0,
+        position: 'absolute',
+        textAlign: 'center',
+        transform: 'translateY(55px)',
+        transition: 'transform 0.2s',
+        borderBottomLeftRadius: theme.shape.borderRadius,
+        borderBottomRightRadius: theme.shape.borderRadius,
+        backgroundColor: '#171717cc',
+        '& span': {
+            color: 'gold',
+            fontSize: 18
+        }
+    }
 }));
 
 function Carousel() {
@@ -86,7 +115,6 @@ function Carousel() {
                         >
                             {`${res[activeStep].category} ${res[activeStep].title}`}
                         </Button>
-
                     </Link>
                 </Paper>
                 <AutoPlaySwipeableViews
@@ -100,12 +128,30 @@ function Carousel() {
                         < div key={index} >
                             {
                                 Math.abs(activeStep - index) <= 2 ? (
-                                    <div>
+                                    <div
+                                        className={classes.imgContainer}
+                                    >
                                         <img
                                             className={classes.img}
                                             src={step.path[step.bannerPath]}
                                             alt={step.title}
                                         />
+                                        <div
+                                            className={clsx(classes.productLink, 'productLink')}
+                                        >
+                                            <Link
+                                                href="/product/[id]"
+                                                as={`/product/${res[activeStep]._id}`}
+                                                passHref
+                                            >
+                                                <Button
+                                                    size="large"
+                                                    component="a"
+                                                >
+                                                    مشاهده محصول
+                                                </Button>
+                                            </Link>
+                                        </div>
                                     </div>
                                 ) : null
                             }
