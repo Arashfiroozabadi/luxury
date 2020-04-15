47b10fa78bd3e9716a7231e097f601080efbfa80
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-04-15 19:31:13 +0430

    rename file
    Signed-off-by: arashfiroozabadi <arashfiroozabadii@gmail.com>

--- pages/management/overview.tsx
+++ pages/management/overview.tsx
@@ -5,7 +5,7 @@ import persianJs from 'persianjs';
 
 
 import ConvertString from '../../components/ConvertString';
-import Chart from './char';
+import Chart from './chart';
 
 function convertValue(v: number) {
   if (v === 0) {
