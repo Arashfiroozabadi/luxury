687f451974afd35715944b3d318ae93a8853a25b
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-05-13 17:57:42 +0100

    create new component

---
+++ components/management/WithNav.tsx
@@ -0,0 +1,93 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import {
+  // eslint-disable-next-line no-unused-vars
+  makeStyles, createStyles, Theme,
+  Paper, Typography,
+} from '@material-ui/core';
+import clsx from 'clsx';
+
+
+import Papeer from '../Paper';
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    transition: 'background-color 250ms linear , color 250ms linear',
+    [theme.breakpoints.only('xs')]: {
+      width: '100%',
+    },
+  },
+  nav: {
+    display: 'flex',
+    justifyContent: 'space-around',
+  },
+  link: {
+    width: '100%',
+    textAlign: 'center',
+  },
+  aTag: {
+    color: theme.palette.text.primary,
+    textDecoration: 'none',
+  },
+  active: {
+    borderBottom: '1px solid blue',
+  },
+}));
+
+
+function WithNav(props:any) {
+  const classes = useStyles();
+  const router = useRouter();
+  const { pathname } = router;
+  const { children } = props;
+
+  return (
+    <Papeer>
+      <Paper
+        className={clsx(classes.nav)}
+      >
+        <Typography
+          variant="h6"
+          color="textSecondary"
+          className={clsx(
+            classes.link,
+            pathname === '/management/chart' ? classes.active : null,
+          )}
+        >
+          <Link href="/management/chart">
+            <a
+              className={classes.aTag}
+            >
+              chart
+            </a>
+          </Link>
+        </Typography>
+        <Typography
+          variant="h6"
+          color="textSecondary"
+          className={clsx(
+            classes.link,
+            pathname === '/management/overview' ? classes.active : null,
+          )}
+        >
+          <Link href="/management/overview">
+            <a
+              className={classes.aTag}
+            >
+              chart
+            </a>
+          </Link>
+        </Typography>
+      </Paper>
+      <div>
+        {children}
+      </div>
+    </Papeer>
+  );
+}
+
+export default WithNav;
