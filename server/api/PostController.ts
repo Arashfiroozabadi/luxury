a667a3263fbb65de08ca2404ec2cb1212e431612
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-05-25 15:37:37 +0430

    complate 'delete' api

--- server/api/PostController.ts
+++ server/api/PostController.ts
@@ -1,17 +1,37 @@
 import express from 'express';
-import { PhotoModel } from '../models';
+import chalk from 'chalk';
+import { PhotoModel, Overview } from '../models';
 
 const PostController = express.Router();
 
 
 PostController.put('/put', async (req, res) => {
-  const { body } = req;
-  await PhotoModel.deleteOne({ _id: body.id }).then((doc) => {
+  const body = req.body.rowData;
+  // console.log(body);
+  await PhotoModel.deleteOne({ _id: body._id }).then((doc) => {
     if (doc.deletedCount === 1) {
-      console.log('document deletet');
+      Overview.updateOne(
+        {
+          name: 'overview',
+          'category.name': body.category,
+        },
+        {
+          $inc: {
+            total: -1,
+            'category.$.value': -1,
+          },
+        }, (err, updatedDoc) => {
+          if (err) return console.log(err);
+          if (updatedDoc.nModified === 1) {
+            return console.log(chalk.green.bold('Chart Data Updated'));
+          }
+          return console.log(chalk.red.bgWhite.bold('Error in Update Chart Data'));
+        },
+      );
+      console.log(`Document with ID::" ${chalk.blue.underline(body._id)} ":: ${chalk.green.bgWhite.bold('Deleted')}`);
       res.sendStatus(200);
     } else {
-      console.log('Document Not Modified');
+      console.log(`Document with ID::" ${chalk.blue.underline(body._id)} ":: ${chalk.yellow.bgWhite.bold('NOT Modified')}`);
       res.sendStatus(304);
     }
   });
