1f5e1c7d585c2eddc0fe5a89ce48d63e93ad0130
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-05-28 16:26:52 +0430

    add delete folder in wiht rimraf

--- server/api/PostController.ts
+++ server/api/PostController.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import chalk from 'chalk';
+import rimraf from 'rimraf';
 import { PhotoModel, Overview } from '../models';
 
 const PostController = express.Router();
@@ -8,7 +9,7 @@ const PostController = express.Router();
 PostController.put('/put', async (req, res) => {
   const body = req.body.rowData;
   // console.log(body);
-  await PhotoModel.deleteOne({ _id: body._id }).then((doc) => {
+  await PhotoModel.deleteOne({ _id: body._id }).then(async (doc) => {
     if (doc.deletedCount === 1) {
       Overview.updateOne(
         {
@@ -21,17 +22,29 @@ PostController.put('/put', async (req, res) => {
             'category.$.value': -1,
           },
         }, (err, updatedDoc) => {
-          if (err) return console.log(err);
+          if (err) return console.log(chalk.red.bgWhite.bold('Error in Update Chart Data'));
           if (updatedDoc.nModified === 1) {
-            return console.log(chalk.green.bold('Chart Data Updated'));
+            console.log(`[ ${chalk.blue.bold('info')} ]${chalk.green.bold('Chart Data Updated')}`);
+          } else {
+            return console.log(`[ ${chalk.yellow.bold('warning')} ] ${chalk.yellow.bgWhite.bold('NOT Modified ChartData')}`);
           }
-          return console.log(chalk.red.bgWhite.bold('Error in Update Chart Data'));
+          return null;
         },
       );
-      console.log(`Document with ID::" ${chalk.blue.underline(body._id)} ":: ${chalk.green.bgWhite.bold('Deleted')}`);
+      console.log(`[ ${chalk.blue.bold('info')} ] Document with ID::" ${chalk.blue.underline(body._id)} ":: ${chalk.green.bgWhite.bold('Deleted')}`);
+      const Path = `./public/static/uploads/${body.category}/${body.title}`;
+      // console.log(process.cwd());
+
+      await rimraf(Path, (err) => {
+        if (err) {
+          return console.log(`[ ${chalk.red.bold('Error')} ] Document directory folder Not Delet`);
+        } return console.log(`[ ${chalk.blue.bold('info')} ] Documet directory Deleted successfully`);
+      });
       res.sendStatus(200);
     } else {
-      console.log(`Document with ID::" ${chalk.blue.underline(body._id)} ":: ${chalk.yellow.bgWhite.bold('NOT Modified')}`);
+      console.log(`[ ${
+        chalk.yellow.bold('warning')
+      } ] Document with ID::" ${chalk.blue.underline(body._id)} ":: ${chalk.yellow.bold('NOT Modified')}`);
       res.sendStatus(304);
     }
   });
