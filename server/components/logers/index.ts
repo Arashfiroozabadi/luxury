7d6fee526b7cd85347b111d6245b12a7d57082fb
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-05-31 13:20:13 +0430

    craete component directory and create first component

---
+++ server/components/logers/index.ts
@@ -0,0 +1,22 @@
+import chalk from 'chalk';
+
+function Loger(
+  type:'info' | 'error' | 'warn',
+  msg?:string,
+):void{
+  switch (type) {
+    case 'info':
+      console.log(`[ ${chalk.blue.bold('info')} ] ${msg}`);
+      break;
+    case 'error':
+      console.log(`[ ${chalk.red.bold('Error')} ] ${msg}`);
+      break;
+    case 'warn':
+      console.log(`[ ${chalk.red.bold('warning')} ] ${msg}`);
+      break;
+    default:
+      break;
+  }
+}
+
+export default Loger;
