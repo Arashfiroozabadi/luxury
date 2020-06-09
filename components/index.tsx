f73554639517febdbcbd32473b9db24f687c6a56
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-06-09 14:41:03 +0430

    export new components

--- components/index.tsx
+++ components/index.tsx
@@ -3,3 +3,9 @@ export * from './Paper';
 
 export { FetchPostList } from './customHooks';
 export * from './customHooks';
+
+export { default as ColorCate } from './ColorCate';
+
+export { default as Div } from './Div';
+
+export { default as NotFound } from './NotFound';
