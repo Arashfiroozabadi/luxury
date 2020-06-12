ae48ad2ed36e77a4d42e614f76834606dde274a5
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-06-12 11:42:47 +0430

    export more components

--- components/index.tsx
+++ components/index.tsx
@@ -15,3 +15,11 @@ export { default as Layout } from './Layout';
 export { default as AppTheme } from './theme';
 
 export { default as RTL } from './RTL';
+
+export { default as Loading } from './loading';
+
+export { default as ListItems } from './search/ListItems';
+
+export { default as ProductCardInfo } from './ProductCardInfo';
+
+export { default as ConvertString } from './ConvertString';
