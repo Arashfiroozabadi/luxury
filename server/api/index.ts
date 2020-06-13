4d9484805a196df851e3d860f091c2deeaed581a
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-06-13 16:04:16 +0430

    delete useless imports and use new api  "UserController"

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,11 +1,8 @@
 import express from 'express';
-import multer from 'multer';
-import mkdirp from 'mkdirp';
 import fs from 'fs';
 
 import loger from '../components/logers';
 import {
-  User,
   Overview,
   Post,
 } from '../models';
@@ -18,6 +15,7 @@ const Banner = require('./banner');
 const Production = require('./production');
 const AllProducts = require('./AllProducts');
 const Search = require('./Search');
+const UserController = require('./UserController');
 
 
 const AccountController = express.Router();
@@ -26,64 +24,6 @@ const liaraClient = new Liara.Storage.Client({
   accessKey: 'Z3SBHOIW4DM5VL9YTHRJF',
   secretKey: 'ejoUJJEe4yqkUYFohJZtYprSxWSqT8wC9Thv9AOBQ',
   endPoint: '5ed778ad1c92cc0011b11ead.liara.space',
-});
-
-const storage = multer.diskStorage({
-  destination(req: any, _files: any, cb) {
-    const dest = `uploads/${req.body.cate}/${req.body.name}`;
-    mkdirp.sync(`statics/${dest}`);
-    cb(null, `statics/${dest}`);
-  },
-  filename(_req: any, file, cb) {
-    cb(null, file.originalname);
-    // console.log(file);
-  },
-});
-function filters(req: any, file: any, cb: any) {
-  // const files = req.files
-
-  if (req.body.name.length === 0 || req.body.cate.length === 0 || req.body.desc.length === 0 || file === undefined) {
-    cb('لطفا تمام فیلد‌ها را وارد کنید', false);
-  }
-  if (file.mimetype !== 'image/jpeg') {
-    cb('file type not allow', false);
-  }
-  cb(null, true);
-}
-const upload = multer({
-  storage,
-  fileFilter: filters,
-});
-
-AccountController.post('/auth', (req: any, res) => {
-  if (req.session.auth) {
-    res.send({ auth: true, user: req.session.auth, msg: 'وارد شدید' });
-  } else {
-    res.send({ auth: false });
-  }
-});
-
-AccountController.post('/login', upload.single('file'), (req: any, res) => {
-  const body = req.body.form;
-
-  if (body.userName.length <= 0) {
-    res.send({ msg: 'نام کاربری را وارد کنید' });
-  } else if (body.pass.length <= 0) {
-    res.send({ msg: 'رمز عبور را وارد کنید' });
-  } else if (req.session.auth === undefined) {
-    User.findOne({ userName: body.userName, pass: body.pass }).then(
-      (resualt: any) => {
-        if (resualt === null) {
-          res.send({ msg: 'نام کاربری و یا رمز عبور اشتباه است' });
-        } else {
-          req.session.auth = { username: body.userName };
-          res.send({ auth: true, userName: resualt.userName, msg: 'وارد شدید' });
-        }
-      },
-    );
-  } else if (req.session.auth.username === body.userName) {
-    res.send({ auth: true, userName: body.userName, msg: 'وارد شدید' });
-  }
 });
 
 AccountController.post('/product', async (req, res) => {
@@ -135,23 +75,6 @@ AccountController.post('/test', (req, res) => {
     console.log('Successfully uploaded.');
     res.send(result);
   });
-  // liaraClient.makeBucket('test1', (err:any) => {
-  //   if (err) return console.log(err);
-  //   return console.log('ok');
-  // });
-  // liaraClient.listBuckets((err:any, buckets:any) => {
-  //   if (err) return console.log(err);
-  //   console.log('buckets :', buckets);
-  // });
-  // liaraClient.presignedUrl('GET', 'images', 'hello-file.jpg', 24 * 60 * 60, (err:any, doc:any) => {
-  //   if (err) {
-  //     return res.send(err);
-  //   }
-  //   // doc.on('data', (d:any) => {
-  //   //   console.log(d);
-  //   // });
-  //   return res.send(doc);
-  // });
   console.log(files.name);
 });
 
@@ -164,4 +87,5 @@ module.exports = [
   Production,
   AllProducts,
   Search,
+  UserController,
 ];
