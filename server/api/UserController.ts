bf92a1b81445bc111306323909ad4ea3deaff614
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-06-15 19:39:10 +0430

    fix login api bugs

--- server/api/UserController.ts
+++ server/api/UserController.ts
@@ -1,7 +1,6 @@
 /* eslint-disable consistent-return */
 import express from 'express';
 import jwt from 'jsonwebtoken';
-
 import Loger from '../components/logers';
 import { User } from '../models';
 import { auth } from '../middleware';
@@ -9,53 +8,47 @@ import { auth } from '../middleware';
 const UserController = express.Router();
 
 
-UserController.post('/login', async (req:any, res) => {
-  const { userName, pass } = await req.body.form;
+UserController.post('/login', async (req:any, res:any) => {
+  const { userName, password } = await req.body.form;
+  if (userName.length === 0 || password.length <= 3) {
+    res.status(400).json({ auth: false, msg: 'نام کاربری یا رمز ورود اشتباه است' });
+  } else {
+    await User.findOne({ userName }, async (errFind, user:any) => {
+      if (errFind) {
+        Loger('error', errFind.message);
+        res.status(500).json({ auth: false, msg: 'server err' });
+      }
+      if (user === null) {
+        res.status(404).json({ auth: false, msg: 'کاربر یافت نشد' });
+      } else {
+        user.comparePassword(password, (errMatchPass:any, isMatch:boolean) => {
+          if (errMatchPass) {
+            Loger('error', errMatchPass);
+            res.status(500).json({ auth: false, msg: 'server err' });
+          }
+          if (isMatch) {
+            const payLoad = {
+              user: { id: user._id },
+            };
+            const key = process.env.token;
 
-  await User.findOne({ userName }, async (errFind, user:any) => {
-    if (errFind) {
-      console.log(errFind);
-      res.status(500).json({ auth: false, msg: 'server err' });
-    }
-    if (user === null) {
-      res.status(404).json({ auth: false, msg: 'کاربر یافت نشد' });
-    } else {
-      user.comparePassword(pass, (errMatchPass:any, isMatch:boolean) => {
-        if (errMatchPass) {
-          Loger('error', errMatchPass);
-          res.status(500).json({ auth: false, msg: 'server err' });
-        }
-        if (isMatch) {
-          const payLoad = {
-            user: { id: user._id },
-          };
-          const key = process.env.token;
-
-          jwt.sign(payLoad, `${key}`, { expiresIn: 10000 }, (errToken, token) => {
-            if (errToken) throw errToken;
-            // req.session.auth = { username: user.userName };
-            res.send({
-              auth: isMatch,
-              userName: user.userName,
-              token,
-              msg: 'شما با موفقیت وارد شدید',
+            jwt.sign(payLoad, `${key}`, { expiresIn: 10000 }, (errToken, token) => {
+              if (errToken) throw errToken;
+              // req.session.auth = { username: user.userName };
+              res.send({
+                auth: isMatch,
+                userName: user.userName,
+                token,
+                msg: 'شما با موفقیت وارد شدید',
+              });
             });
-          });
-        } else {
-          res.status(404).json({ auth: isMatch, msg: 'رمز ورود اشتباه است' });
-        }
-      });
-    }
-  });
-  //   const newUser = new User({
-  //     userName: name,
-  //     password: pass,
-  //     type: 'admin',
-  //   });
-
-  //   newUser.save().then((doc) => {
-  //     console.log(doc);
-  //   });
+          } else {
+            res.status(404).json({ auth: isMatch, msg: 'رمز ورود اشتباه است' });
+          }
+        });
+      }
+    });
+  }
 });
 
 UserController.get('/admininfo', auth, async (req:any, res) => {
@@ -68,7 +61,7 @@ UserController.get('/admininfo', auth, async (req:any, res) => {
       });
     }).catch((e) => {
       res.status(500).json({ msg: 'server err', e });
-      console.log(e);
+      Loger('error', e);
     });
   } catch (error) {
     res.status(401).json({ msg: 'احراز هویت انجام نشد' });
