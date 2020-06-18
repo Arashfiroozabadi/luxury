023cb053520a95a3d8f88d0a9c7d07e773f832c6
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-06-18 20:47:32 +0430

    use redux and try to fix bug after logout

--- pages/management/index.tsx
+++ pages/management/index.tsx
@@ -8,6 +8,8 @@ import {
 import clsx from 'clsx';
 import Axios from 'axios';
 import { Visibility, VisibilityOff } from '@material-ui/icons';
+import { useSelector, useDispatch } from 'react-redux';
+
 import {
   AppTheme, Layout, Div, Paper, RTL, Loading, AdminPanel,
 } from '../../components';
@@ -70,7 +72,7 @@ interface formDataProps{
 
 function Management() {
   const classes = useStyles();
-  const [auth, setAuth] = useState<boolean>();
+  // const [auth, setAuth] = useState<boolean>();
   const [isLoading, setisLoading] = useState<boolean>(false);
   const [showPass, setShowPass] = useState<boolean>(false);
   const [data, setData] = useState<dataProps>({
@@ -82,10 +84,15 @@ function Management() {
     userName: '',
     password: '',
   });
-  const [Error, setError] = useState({
-    err: false,
-    msg: '',
-  });
+  // const [Error, setError] = useState({
+  //   err: false,
+  //   msg: '',
+  // });
+
+  const dispatch = useDispatch();
+  const auth = useSelector((state:any) => state.auth);
+  const Error = useSelector((state:any) => state.err);
+  console.log(Error);
 
 
   function handleSubmit(e:any) {
@@ -93,25 +100,36 @@ function Management() {
     const passCheck = FormData.password.length;
     if (userCheck <= 0) {
       e.preventDefault();
-      return setError({ err: true, msg: 'نام کاربری را وارد کنید' });
+      return dispatch({ type: 'err', err: { err: true, msg: 'نام کاربری را وارد کنید' } });
+      // return setError({ err: true, msg: 'نام کاربری را وارد کنید' });
     }
     if (passCheck <= 3) {
       e.preventDefault();
-      return setError({ err: true, msg: 'رمز ورود نباید کمتر از ۴ حرف باشد' });
+      return dispatch({ type: 'err', err: { err: true, msg: 'رمز ورود نباید کمتر از ۴ حرف باشد' } });
+      // return setError({ err: true, msg: 'رمز ورود نباید کمتر از ۴ حرف باشد' });
     }
     e.preventDefault();
     const fetchData = async () => {
       setisLoading(true);
-      await Axios.post('/api/login', { form: FormData }).then((result) => {
-        setError({ err: false, msg: result.data.msg });
+      await Axios.post('/api/login', { form: FormData }).then((result:any) => {
+        // setError({ err: false, msg: result.data.msg });
+        dispatch({ type: 'err', err: { err: false, msg: result.data.msg } });
+
         localStorage.setItem('token', result.data.token);
+        setData({
+          userName: result.data.userName,
+          type: result.data.type,
+          msg: result.data.msg,
+        });
         setTimeout(() => {
           setisLoading(false);
-          setAuth(result.data.auth);
+          // setAuth(result.data.auth);
+          dispatch({ type: 'authStatus', auth: true });
         }, 2000);
       }).catch((err) => {
         setisLoading(false);
-        setError({ err: true, msg: err.response.data.msg });
+        // setError({ err: true, msg: err.response.data.msg });
+        dispatch({ type: 'err', err: { err: true, msg: err.response.data.msg } });
       });
     };
 
@@ -119,15 +137,26 @@ function Management() {
   }
 
   function handleOnChangeInput(e:any) {
+    console.log(auth);
+
+    if (!auth) {
+      setFormData({ userName: '', password: '' });
+    }
     setFormData({ ...FormData, [e.target.name]: e.target.value });
-    setError({ err: false, msg: '' });
+    // setError({ err: false, msg: '' });
+    dispatch({ type: 'err', err: { err: false, msg: '' } });
   }
 
   function handleShowPass() {
     setShowPass(!showPass);
   }
 
+
   useEffect(() => {
+    if (!auth) {
+      // setError({ err: true, msg: '' });
+      setFormData({ userName: '', password: '' });
+    }
     const userToken = localStorage.getItem('token');
     const fetchData = async () => {
       setisLoading(true);
@@ -136,25 +165,56 @@ function Management() {
           token: userToken,
         },
       }).then((result) => {
-        setError({ err: false, msg: result.data.msg });
+        // setError({ err: false, msg: result.data.msg });
+        dispatch({ type: 'err', err: { err: false, msg: result.data.msg } });
+
         setTimeout(() => {
           // setisLoading(false);
-          setAuth(result.data.auth);
+          // setAuth(result.data.auth);
+          dispatch({ type: 'authStatus', auth: true });
         }, 3000);
         setData(result.data);
       }).catch((e) => {
         setisLoading(false);
-        setError({ err: true, msg: e.response.data.msg });
+        dispatch({ type: 'authStatus', auth: false });
+        // setError({ err: true, msg: e.response.data.msg });
+        dispatch({ type: 'err', err: { err: true, msg: e.response.data.msg } });
         console.error(e);
       });
     };
     fetchData();
-
-    return () => {
-
-    };
   }, []);
 
+  useEffect(() => () => {
+    console.log('cleaned up');
+  }, []);
+
+  if (auth) {
+    return (
+      <AppTheme>
+        <Layout>
+          <Container className={classes.container}>
+            <Div className={clsx(classes.header)}>
+              <Typography
+                variant="h4"
+                component="h1"
+              >
+                <Box>
+                  مدیریت
+                </Box>
+              </Typography>
+            </Div>
+            <Div>
+              <AdminPanel
+                userName={data.userName}
+                userType={data.type}
+              />
+            </Div>
+          </Container>
+        </Layout>
+      </AppTheme>
+    );
+  }
   return (
     <AppTheme>
       <Layout>
@@ -170,79 +230,70 @@ function Management() {
             </Typography>
           </Div>
           <Div>
-            {!auth
-              ? (
-                <div>
-                  <div className={clsx(classes.formRoot)}>
-                    <Paper className={clsx(classes.formPaper)}>
-                      <form
-                        className={clsx(classes.form)}
-                        onSubmit={(e) => handleSubmit(e)}
+            <div>
+              <div className={clsx(classes.formRoot)}>
+                <Paper className={clsx(classes.formPaper)}>
+                  <form
+                    className={clsx(classes.form)}
+                    onSubmit={(e) => handleSubmit(e)}
+                  >
+                    <RTL>
+                      <TextField
+                        label="نام کاربری"
+                        name="userName"
+                        helperText=""
+                        onInvalid={(e:any) => e.target.setCustomValidity('نام کاربری را وارد کنید')}
+                        onInput={(e:any) => e.target.setCustomValidity('')}
+                        required={Error.err}
+                        onChange={(e) => handleOnChangeInput(e)}
+                      />
+                      <TextField
+                        label="رمز ورود"
+                        name="password"
+                        type={showPass ? 'text' : 'password'}
+                        helperText=""
+                        onInvalid={(e:any) => e.target.setCustomValidity('رمز ورود را وارد کنید')}
+                        onInput={(e:any) => e.target.setCustomValidity('')}
+                        required={Error.err}
+                        onChange={(e) => handleOnChangeInput(e)}
+                        InputProps={{
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <IconButton
+                                onClick={() => handleShowPass()}
+                              >
+                                {showPass ? <Visibility /> : <VisibilityOff />}
+                              </IconButton>
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+                      <Button
+                        className={classes.submitButton}
+                        disabled={(isLoading || Error.err)}
+                        type="submit"
+                        variant="outlined"
                       >
-                        <RTL>
-                          <TextField
-                            label="نام کاربری"
-                            name="userName"
-                            helperText=""
-                            onInvalid={(e:any) => e.target.setCustomValidity('نام کاربری را وارد کنید')}
-                            onInput={(e:any) => e.target.setCustomValidity('')}
-                            required={Error.err}
-                            onChange={(e) => handleOnChangeInput(e)}
-                          />
-                          <TextField
-                            label="رمز ورود"
-                            name="password"
-                            type={showPass ? 'text' : 'password'}
-                            helperText=""
-                            onInvalid={(e:any) => e.target.setCustomValidity('رمز ورود را وارد کنید')}
-                            onInput={(e:any) => e.target.setCustomValidity('')}
-                            required={Error.err}
-                            onChange={(e) => handleOnChangeInput(e)}
-                            InputProps={{
-                              endAdornment: (
-                                <InputAdornment position="end">
-                                  <IconButton
-                                    onClick={() => handleShowPass()}
-                                  >
-                                    {showPass ? <Visibility /> : <VisibilityOff />}
-                                  </IconButton>
-                                </InputAdornment>
-                              ),
-                            }}
-                          />
-                          <Button
-                            className={classes.submitButton}
-                            disabled={(isLoading || Error.err)}
-                            type="submit"
-                            variant="outlined"
-                          >
-                            ورود
-                          </Button>
-                          <FormHelperText
-                            className={clsx(
-                              classes.formTextHelper,
-                              Error.err ? null : classes.success,
-                            )}
-                            error={Error.err}
-                            variant="outlined"
-                          >
-                            {Error.msg}
-                          </FormHelperText>
-                        </RTL>
-                      </form>
-                      {isLoading
-                        ? <Loading className={classes.loading} size={30} />
-                        : null}
-                    </Paper>
-                  </div>
-                </div>
-              )
-              : (
-                <AdminPanel
-                  userName={data.userName}
-                  userType={data.type}
-                />
-              )}
+                        ورود
+                      </Button>
+                      <FormHelperText
+                        className={clsx(
+                          classes.formTextHelper,
+                          Error.err ? null : classes.success,
+                        )}
+                        error={Error.err}
+                        variant="outlined"
+                      >
+                        {Error.msg}
+                      </FormHelperText>
+                    </RTL>
+                  </form>
+                  {isLoading
+                    ? <Loading className={classes.loading} size={30} />
+                    : null}
+                </Paper>
+              </div>
+            </div>
           </Div>
         </Container>
       </Layout>
