ad8ee7b3ca176682ed07521c3bb060063bba14cf
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-06-19 18:01:00 +0430

    update user auth api & alert msg

--- pages/management/chart.tsx
+++ pages/management/chart.tsx
@@ -7,10 +7,11 @@ import Axios from 'axios';
 import Router from 'next/router';
 import {
 // eslint-disable-next-line no-unused-vars
-  makeStyles, createStyles, Theme, Button,
+  makeStyles, createStyles, Theme, Button, Typography, Box,
 } from '@material-ui/core';
 
-import { Loading } from '../../components';
+import { Warning } from '@material-ui/icons';
+import { Loading, Paper } from '../../components';
 import AppTheme from '../../components/theme';
 import Layout from '../../components/Layout';
 import WithNav from '../../components/management/WithNav';
@@ -24,6 +25,18 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
       width: '100%',
     },
   },
+  unAuth: {
+    textAlign: 'center',
+  },
+  alertMsg: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  alertIcon: {
+    color: 'gold',
+    margin: '0px 10px',
+  },
   loading: {
     display: 'flex',
     marginTop: 40,
@@ -33,70 +46,82 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
 
 function Chart() {
   const classes = useStyles();
-  const [data, setdata] = useState<any>({});
+
+  const [isLoading, setisLoading] = useState(true);
 
   const dispatch = useDispatch();
   const auth = useSelector((state:any) => state.auth);
-  const [isLoading, setisLoading] = useState(true);
 
   const handleGoHome = () => {
     Router.push('/');
   };
   useEffect(() => {
-    if (!auth.auth) {
-      const fetchData = async () => {
-        setisLoading(true);
-        await Axios.post('/api/auth').then((result:any) => {
-          setdata(result.data);
-          localStorage.setItem('auth', result.data.auth);
-          dispatch({ type: 'authStatus', auth: result.data });
-        }).catch((err) => {
-          console.log(err);
-          setisLoading(false);
-        });
-      };
-      fetchData();
-      setisLoading(false);
-    } else {
-      setisLoading(false);
-    }
+    const tokenKey = localStorage.getItem('token');
+    const fetchData = async () => {
+      setisLoading(true);
+      await Axios.get('/api/admininfo', {
+        headers: {
+          token: tokenKey,
+        },
+      }).then((result:any) => {
+        dispatch({ type: 'authStatus', auth: true });
+        dispatch({ type: 'login', loginForm: { userName: result.data.userName, password: '' } });
+        setisLoading(false);
+      }).catch((err) => {
+        console.log(err);
+        setisLoading(false);
+      });
+    };
+    fetchData();
   }, []);
 
   useEffect(() => () => {
     console.log('cleaned up');
   }, []);
 
-  if (!auth.auth) {
-    return (
-      <AppTheme>
-        <Layout>
-          <h1>
-            you are not admin go back
-            {data.auth}
-          </h1>
-          <Button
-            onClick={handleGoHome}
-          >
-            back to home
-          </Button>
-        </Layout>
-      </AppTheme>
-    );
-  }
-  if (isLoading) {
-    return (
-      <AppTheme>
-        <Layout>
-          <Loading className={classes.loading} size={80} />
-        </Layout>
-      </AppTheme>
-    );
-  } return (
+  return (
     <AppTheme>
       <Layout>
-        <WithNav className={classes.root}>
-          <Overview />
-        </WithNav>
+        {!auth
+          ? (
+            <Paper className={classes.unAuth}>
+              {isLoading ? (<Loading className={classes.loading} size={50} />)
+                : (
+                  <>
+                    <Typography
+                      variant="h5"
+                      component="h5"
+                    >
+                      <Box
+                        className={classes.alertMsg}
+                        component="p"
+                        textAlign="center"
+                      >
+                        <Warning className={classes.alertIcon} fontSize="large" />
+                        فقط مدیران میتوانند به قسمت دسترسی داشته باشند
+                        <Warning className={classes.alertIcon} fontSize="large" />
+                      </Box>
+                    </Typography>
+                    <Button
+                      onClick={handleGoHome}
+                      variant="outlined"
+                    >
+                      بازگشت به صفحه اصلی
+                    </Button>
+                  </>
+                )}
+            </Paper>
+          )
+          : (
+            <>
+              {isLoading ? (<Loading className={classes.loading} size={50} />)
+                : (
+                  <WithNav className={classes.root}>
+                    <Overview />
+                  </WithNav>
+                )}
+            </>
+          )}
       </Layout>
     </AppTheme>
   );
