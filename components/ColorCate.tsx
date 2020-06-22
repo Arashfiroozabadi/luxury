a2c62f2f327d77992bfa416ed3adcd2cbf448a0f
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-06-22 16:08:22 +0430

    fix value bug

--- components/ColorCate.tsx
+++ components/ColorCate.tsx
@@ -2,11 +2,11 @@ function ColorCate(cate:string) {
   switch (cate) {
     case 'rahati':
       return '#e8e230';
-    case 'rahatil':
+    case 'rahati-l':
       return '#ec407a';
-    case 'servicekhab':
+    case 'service-khab':
       return '#ab47bc';
-    case 'naharkhori':
+    case 'nahar-khori':
       return '#01b075';
     case 'console':
       return '#ff5722';
