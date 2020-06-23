7d3ce420b9624a0ad2faaf37ee2c397c9ea46a26
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-06-23 16:03:18 +0430

    change .env const name

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -5,7 +5,7 @@ function auth(req: any, res:any, next:any) {
   const token = req.header('token');
   if (!token) return res.status(401).json({ msg: 'Auth Error' });
   try {
-    const key = process.env.token;
+    const key = process.env.tokenKey;
     const decoded:any = jwt.verify(token, `${key}`);
     req.user = decoded.user;
     return next();
