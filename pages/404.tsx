94a4038e316ed0b89c76fe496e642ebd284fd15f
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-06-25 15:29:36 +0430

    complate 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { AppTheme, Layout } from 'components';
 
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    flexGrow: 1,
+    textAlign: 'center',
+    minHeight: 167,
+    padding: theme.spacing(3),
+    [theme.breakpoints.down('sm')]: {
+      minHeight: 10,
+      padding: theme.spacing(1),
+    },
+  },
+  pagePath: {
+    color: '#2771e0',
+  },
+  errCode: {
+    fontSize: '2rem',
+    marginBottom: 0,
+  },
+}));
 
-export default function Custom404() {
+export default function Custom404(): JSX.Element {
+  const classes = useStyles();
   return (
-    <main>
-      <h1>404 - Page Not Found</h1>
-    </main>
+    <AppTheme>
+      <Layout>
+        <main className={classes.root}>
+          <h1>
+            صفحه مورد نظر یافت نشد
+          </h1>
+          <code className={classes.errCode}>404</code>
+        </main>
+      </Layout>
+    </AppTheme>
   );
 }
