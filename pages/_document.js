c8be5588904f477ffea8c9d365eccf915324eefa
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-06-26 16:29:55 +0430

    add css link for mapbox

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ class MyDocument extends Document {
           />
           {/* PWA primary color */}
           <meta name="theme-color" content={themeDark.palette.primary.main} />
+          <link href="https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.css" rel="stylesheet" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
