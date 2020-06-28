138ee8ca01e04d45291244d95b8373ff450d5fa9
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-06-28 13:24:26 +0430

    add meta tag for product

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -12,6 +12,7 @@ import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Layout from '../../components/Layout';
 import Product from '../../components/Product';
 import AppTheme from '../../components/theme';
+import ConvertString from '../../components/ConvertString';
 
 const useStyles = makeStyles((theme) => createStyles({
   root: {
@@ -54,8 +55,31 @@ function Post() {
       <Layout>
         <Head>
           <title>
-            {`مدل ${res.title}`}
+            {`${ConvertString(res.category)} ${res.title}`}
           </title>
+          <meta
+            property="og:url"
+            content={`http://luxury.ir/product/${res._id}`}
+          />
+          <meta
+            property="og:image"
+            content={
+              res.imagePath
+                ? res.imagePath[0] : null
+            }
+          />
+          <meta
+            property="og:image:secure_url"
+            content={
+              res.imagePath
+                ? res.imagePath[0] : null
+            }
+          />
+          <meta property="og:image:type" content="image/jpeg" />
+          <meta property="og:image:width" content="400" />
+          <meta property="og:image:height" content="300" />
+          <meta property="og:image:alt" content={res.title} />
+
         </Head>
         <Container className={classes.root}>
           <Grid container justify="center">
