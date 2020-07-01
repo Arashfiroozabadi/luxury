4098cf3be572aa7193a98634601eae8e80078a1d
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-07-01 11:45:34 +0430

    add next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 const withPlugins = require('next-compose-plugins');
 const withPWA = require('next-pwa');
+const optimizedImages = require('next-optimized-images');
 
 module.exports = process.env.NODE_ENV === 'development'
   ? withPlugins([
     withPWA({}),
+    optimizedImages({
+      optimizeImagesInDev: true,
+    }),
   ], {
     env: {
       tokenKey: 'djdammasdesjuskesmuskjkjds',
@@ -11,6 +15,9 @@ module.exports = process.env.NODE_ENV === 'development'
   })
   : withPlugins([
     withPWA({}),
+    optimizedImages({
+      optimizeImagesInDev: true,
+    }),
   ], {
     env: {
       tokenKey: 'djdammasdesjuskesmuskjkjds',
